88a62a2fa50c9c191a96bd600d45d50aa9454882
PhungNhuY <yphung282@gmail.com>
2022-10-31 10:47:27 +0700

    update app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const errorHandle = require("./middleware/errorHandler");
+require('dotenv').config();
+
+const app = express();
+port = process.env.PORT;
+
+// connect DB
+const db = require('./config/database');
+db.connect();
+
+app.use(express.urlencoded({
+    extended: true,
+}));
+app.use(express.json());
+
+// router
+const route = require('./routers');
+route(app);
+
+// error handle
+app.use(errorHandle);
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
