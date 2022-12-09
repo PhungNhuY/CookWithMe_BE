89b6a0cd8ef709925e94eb2d3ffb7afa4c45b4a5
PhungNhuY <yphung282@gmail.com>
2022-12-10 00:05:45 +0700

    update populate for recommend api

--- controllers/recommendController.js
+++ controllers/recommendController.js
@@ -1,36 +1,41 @@
 const RecommendModel = require('../models/recommendModel');
+const UserModel = require('../models/userModel');
 const PostModel = require('../models/postModel');
+const CategoryModel = require('../models/categoryModel');
 const { msgEnum } = require('../enums/message.enum');
 const { statusEnum } = require('../enums/status.enum');
 const { codeEnum } = require('../enums/status-code.enum');
 
 class RecommendController {
     async getRecommend(req, res, next){
+        console.log("---RecommendController.getRecommend");
         try {
             let posts = await PostModel.aggregate([
                 [ { $sample: { size: 5 } } ]
-            ])
+            ]);
 
-            res.status(codeEnum.SUCCESS).json({
+            await UserModel.populate(posts, {path:"author"});
+            await CategoryModel.populate(posts, {path:"categories"});
+
+            return res.status(codeEnum.SUCCESS).json({
                 status: statusEnum.SUCCESS,
                 data: posts
-            })
+            });
         } catch (error) {
-            next(error)
+            next(error);
         }
         
     }
     createRecommend(req, res, next){
-        console.log("---RecommendController.deletePost");
+        console.log("---RecommendController.createRecommend");
         
     }
     updateRecommend(req, res, next){
-        console.log("---RecommendController.deletePost");
+        console.log("---RecommendController.updateRecommend");
         
     }
     deleteRecommend(req, res, next){
-        console.log("---RecommendController.deletePost");
-        
+        console.log("---RecommendController.deleteRecommend");
     }
 }
 
